c523710236df65701f4abbb0140d7d51a63ebd4f
Niklas <niklas.fertig23@gmail.com>
2024-05-05 13:54:39 +0200

    Cleaning the Code (remove print)

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:tutorial_flutter/views/login_view.dart';
 import 'package:tutorial_flutter/views/register_view.dart';
 import 'package:tutorial_flutter/views/verify_email_view.dart';
 import 'firebase_options.dart';
-import 'dart:developer' as devtools show log; //you can now only use "log" with "devtools.log"
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +18,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
