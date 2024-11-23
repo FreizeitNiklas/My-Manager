945d54401c24f581f164f4907b30a8ed989ccd8a
Niklas <niklas.fertig23@gmail.com>
2024-11-23 10:05:31 +0100

    Chapter 47

--- lib/services/cloud/firebase_cloud_storage.dart
+++ lib/services/cloud/firebase_cloud_storage.dart
@@ -21,7 +21,7 @@ class FirebaseCloudStorage {
   Future<void> updateNote({
     required String documentId,
     required String text,
-}) async {
+  }) async {
     try {
       // Versucht, die Notiz in Firestore zu aktualisieren
       await notes.doc(documentId).update({textFieldName: text});
@@ -31,29 +31,15 @@ class FirebaseCloudStorage {
   }
 
   // Gibt einen Stream von Notizen für einen bestimmten Benutzer zurück
-  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) => // Stream sendet jedes mal, wenn die Datenbank aktualisiert wird
-    notes.snapshots().map((event) => // Snapshot von jeder Änderung / die map macht aus jedem "event" (= neuer Snapshot) eine sammlung von Iterable<CloudNote> Objekten
-        event.docs.map((doc) => // bei jedem "event" werden alle Dokumente ("docs") zu einer Liste zusammen gefasst ("map") / eine Liste aus "doc" Objekten
-            CloudNote.fromSnapshot(doc)) // Umwandlung in ein nutzbares Datenmodell / "doc" wird durch "fromSnapshot" übergeben und zu "CloudNote" Objekten gemacht
-    .where((note) => note.ownerUserId == ownerUserId)); // filtern der "note" nach den "note" bei denen der "ownerUserId" übereinstimmt
-
-  // Methode zum Abrufen aller Notizen eines bestimmten Benutzers
-  Future<Iterable<CloudNote>> getNotes({required String ownerUserId}) async{ //Hab hier einen Error?!
-    try {
-      // Abrufen aller Dokumente aus der 'notes'-Sammlung, die zum angegebenen Benutzer gehören
-      return await notes
-          .where(
-            ownerUserIdFieldName,
-            isEqualTo: ownerUserId
-          )
-          .get() // Führt die Abfrage aus und holt die entsprechenden Dokumente
-          .then( // Nach dem Abrufen der Dokumente wird eine Funktion ausgeführt
-            (value) => value.docs.map((doc) => CloudNote.fromSnapshot(doc)),
-            // "(value)" ist das Ergebnis von ".get" / Mapped jedes Dokument (doc) in der Liste der abgerufenen Dokumente (value.docs)
-         );
-    } catch (e) {
-      throw CouldNotGetAllNotesException();
-    }
+  Stream<Iterable<CloudNote>> allNotes({required String ownerUserId}) {
+    final allNotes = notes
+        .where(ownerUserIdFieldName, isEqualTo: ownerUserId)
+        .snapshots()
+        .map((event) => event
+            .docs
+            .map((doc) => CloudNote.fromSnapshot( // Snapshot von jeder Änderung / die map macht aus jedem "event" (= neuer Snapshot) eine sammlung von Iterable<CloudNote> Objekten
+                doc))); // bei jedem "event" werden alle Dokumente ("docs") zu einer Liste zusammen gefasst ("map") / eine Liste aus "doc" Objekten
+    return allNotes;
   }
 
   // Erstellen einer neuen Notiz
@@ -74,5 +60,6 @@ class FirebaseCloudStorage {
   static final FirebaseCloudStorage _shared =
       FirebaseCloudStorage._sharedInstance();
   FirebaseCloudStorage._sharedInstance(); // Privater Konstruktor zur Erstellung der Instanz
-  factory FirebaseCloudStorage() => _shared; // Factory-Konstruktor, der die gleiche Instanz zurückgibt => talks with "_shared"
+  factory FirebaseCloudStorage() =>
+      _shared; // Factory-Konstruktor, der die gleiche Instanz zurückgibt => talks with "_shared"
 } // creating a singleton
